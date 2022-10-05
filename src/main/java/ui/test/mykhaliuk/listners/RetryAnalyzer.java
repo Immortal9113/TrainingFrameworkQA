149fd420811b08c77260321bb7032183dddb510d
dmoke <dmytromykhaliukk@gmail.com>
2022-10-05 12:37:27 +0300

    ui testing framework setup

---
+++ src/main/java/ui/test/mykhaliuk/listners/RetryAnalyzer.java
@@ -0,0 +1,20 @@
+package ui.test.mykhaliuk.listners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryAnalyzer implements IRetryAnalyzer {
+
+    private static final int MAX_RETRY_COUNT = 1;
+    private int retryCount = 0;
+
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < MAX_RETRY_COUNT) {
+            System.out.printf("Retrying test %s for the %d time(s).%n", result.getName(), retryCount + 1);
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+}
---
+++ src/main/java/ui/test/mykhaliuk/pages/udemyPages/HomePage.java
@@ -0,0 +1,72 @@
+package ui.test.mykhaliuk.pages.udemyPages;
+
+import io.qameta.allure.Step;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import ui.test.mykhaliuk.elements.HomeElements;
+
+import java.util.Objects;
+
+public class HomePage extends HomeElements {
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void hoverOver(WebElement e) {
+        Actions action = new Actions(driver);
+        action.moveToElement(e).perform();
+    }
+
+    @Step("Search for keyword")
+    public HomePage searchFor(String keywords) {
+        getSearchForm().sendKeys(keywords, Keys.RETURN);
+        return this;
+    }
+
+    @Step("Clicking heading bar close button & Hovering over categories menu button")
+    public HomePage expandCategoriesMenu() {
+        this.closeHeadingBar();
+        this.hoverOver(getCategoriesButton());
+        return this;
+    }
+
+    public void closeHeadingBar() {
+        getHeadingBarCloseButton().click();
+    }
+
+    @Step("Hovering over development category item")
+    public HomePage selectDevelopmentCategory() {
+        hoverOver(getDevCategory());
+        return this;
+    }
+
+    @Step("Click web dev category item")
+    public HomePage clickWebDevCategory() {
+        getWebDevCategory().click();
+        return this;
+    }
+
+    @Step("Click data science category item")
+    public HomePage clickDataScienceCategory() {
+        getDataScienceCategory().click();
+        return this;
+    }
+
+    @Step("Asserting web dev url")
+    public boolean isWebDevPage() {
+        String actualUrl = this.driver.getCurrentUrl();
+        String targetUrl = "https://www.udemy.com/courses/development/web-development/";
+        return Objects.equals(actualUrl, targetUrl);
+    }
+
+    @Step("Asserting data science url")
+    public boolean isDataSciencePage() {
+        String actualUrl = this.driver.getCurrentUrl();
+        String targetUrl = "https://www.udemy.com/courses/development/data-science/";
+        return Objects.equals(actualUrl, targetUrl);
+    }
+
+}
