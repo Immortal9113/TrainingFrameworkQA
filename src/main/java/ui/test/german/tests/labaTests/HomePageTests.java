7782b963234ae07b6764f16a5f99c079dcd7f3db
TetianaGerman <tanya.german95@gmail.com>
2022-10-05 14:52:06 -0700

    germanChangePackages

---
+++ src/main/java/ui/test/german/tests/labaTests/HomePageTests.java
@@ -0,0 +1,23 @@
+package ui.test.german.tests.labaTests;
+
+import org.testng.annotations.Test;
+import ui.test.german.pages.labaPages.HomePage;
+import ui.test.common.*;
+
+public class HomePageTests extends BaseTest {
+ public final static String homePageUrl = "https://l-a-b-a.com/uk";
+
+ @Test
+ public void checkBtnOnlineCourses() {
+  HomePage homePage = new HomePage(driver);
+  openUrl(homePageUrl);
+  homePage.getClickOnlineCoursesBtn().click();
+ }
+
+ @Test
+ public void checkLogoBtn() {
+  HomePage homePage = new HomePage(driver);
+  openUrl(homePageUrl);
+  homePage.getClickLogo().click();
+ }
+}
